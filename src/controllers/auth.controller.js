083f99bec4fdeeb8045ea6bee8a94ca39d372428
aarav-agn <aarav.agnihotri@s.amity.edu>
2025-03-15 21:16:35 +0530

    .

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,16 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.models.js";
 import bcrypt from "bcrypt";
 
-export const signup = (req, res) => {
+export const signup = async (req, res) => {
 const { fullName,email , password } = req.body;    
     try {
+        if (!fullName || !email || !password ) {
+            return res.status(400).json({ message: "All fields are required"});
+        }
+        
         if (password.length < 6) {
-            return res.status(400).json({"Password must be at least 6 characters long"});
+            return res.status(400).json({message:"Password must be at least 6 characters long"});
         }     
         
         const user = await User.findOne({email});
@@ -22,13 +27,22 @@ const { fullName,email , password } = req.body;
         });
 
         if (newUser) {
-            //generate jwt token here 
+
+            generateToken(newUser._id, res);
+            await newUser.save();
+            
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
         } else {
             return res.status(400).json({message:"User not created"});
         }
-
     } catch (error){
-
+        console.log("Errorn in signup controller", error.message);
+        res.status(500).json({message: "Internal Server Error" })
     }
     
 };
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,10 +1,17 @@
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
 
 export const generateToken = (userId, res ) => {
 
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {
         expiresIn: "7d",
     });
+
+    res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "development" ,
+        maxAge: 7 * 24 * 60 * 60 * 1000, //MS
+        sameSite: "strict", //CSRFattacks cross site request forgery attacks 
+    }) 
+
+    return token;
 }
