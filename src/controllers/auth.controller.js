e0e377e7e227e488eeeeeacfb7a0e06713c9fd0f
aarav-agn <aarav.agnihotri@s.amity.edu>
2025-03-15 09:30:59 +0530

    buuildig up sign up

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,35 @@
+import User from "../models/user.models.js";
+import bcrypt from "bcrypt";
+
 export const signup = (req, res) => {
-    res.send("signup route");
+const { fullName,email , password } = req.body;    
+    try {
+        if (password.length < 6) {
+            return res.status(400).json({"Password must be at least 6 characters long"});
+        }     
+        
+        const user = await User.findOne({email});
+        
+        if (user) return res.status(400).json({message:"User already exists"});
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password,
+        });
+
+        if (newUser) {
+            //generate jwt token here 
+        } else {
+            return res.status(400).json({message:"User not created"});
+        }
+
+    } catch (error){
+
+    }
     
 };
 
